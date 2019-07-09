395f7e59c77085f8d030b03950987d02e5e994d9
gfrog <gfrog@gfrog.net>
2019-07-09 19:53:18 +0800

    pelican: Ignore drafts when publishing

--- publishconf.py
+++ publishconf.py
@@ -24,6 +24,8 @@ GMAPS_API_KEY = "AIzaSyB63YDb3SVNe84KeNXPh5zIjJ_uwr5vsII"
 
 DELETE_OUTPUT_DIRECTORY = True
 
+IGNORE_FILES = ['.#*', '*drafts*']
+
 # Following items are often useful when publishing
 
 DISQUS_SITENAME = "gfrog"
